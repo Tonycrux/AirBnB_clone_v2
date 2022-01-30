13b4cd0058a239b7ce0a636e3522673b789289fe
root <sunnexbanty@gmail.com>
2022-01-30 13:00:20 -0800

    m

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
 """Python is cool!"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_route():
-    """hello_route"""
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """hbnb"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """c_route"""
-    return "C {}".format(text.replace("_", " "))
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """python is cool"""
-    text = text.replace("_"," ")
+    """Displays 'Python' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
